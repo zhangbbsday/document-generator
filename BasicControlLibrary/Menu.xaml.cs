116a4fd1ee7bd32dd0f7546181a80dc306ed7a41
zhangbbsday <1291238048@qq.com>
2019-12-24 14:30:08 +0800

    Try to add commands.

--- BasicControlLibrary/Menu.xaml.cs
+++ BasicControlLibrary/Menu.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -10,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using XMLHandle;
+using Forms = System.Windows.Forms;
 
 namespace BasicControlLibrary
 {
@@ -21,6 +24,51 @@ namespace BasicControlLibrary
         public Menu()
         {
             InitializeComponent();
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            XMLDocumentationHandle documentation = new XMLDocumentationHandle(XMLDefault.XMLMarksDefault);
+            documentation.Open(Open());
+            documentation.Split(Save());
+            MessageBox.Show("分割完成!");
+        }
+
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("新建!");
+        }
+
+        private string Open()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Title = "打开文件",
+                DefaultExt = ".xml",
+                Filter = "XML文档(.xml)|*.xml|主文档文件(.xmlmain)|*.xmlmain"
+            };
+
+            if (openFileDialog.ShowDialog() ?? false == true)
+                return openFileDialog.FileName;
+            return null;
+        }
+
+        private string Save()
+        {
+            using Forms::FolderBrowserDialog folderBrowserDialog = new Forms::FolderBrowserDialog
+            {
+                Description = "选择保存路径",
+                ShowNewFolderButton = true
+            };
+
+            if (folderBrowserDialog.ShowDialog() == Forms::DialogResult.OK)
+            {
+                string path = folderBrowserDialog.SelectedPath;
+                if (!path.EndsWith("\\"))
+                    path = path.Insert(path.Length, @"\");
+                return path;
+            }
+            return null;
         }
     }
 }
